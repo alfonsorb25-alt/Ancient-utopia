78ae934a7be80e98fc589953938d48facaee932c
alfonsorb25-alt <alfonsorb25@gmail.com>
2026-06-23 00:26:53 -0700

    Create WorldManager.js

---
+++ docs/src/core/src/world/WorldManager.js
@@ -0,0 +1,10 @@
+export class WorldManager {
+    constructor() {
+        this.worldSize = 15000;
+        this.chunkSize = 500;
+    }
+
+    generate() {
+        console.log("Generating world...");
+    }
+}
