84ea94a8f6085c10f83805b9cc532e5f0d72657f
alfonsorb25-alt <alfonsorb25@gmail.com>
2026-06-23 00:26:21 -0700

    Create PlayerController.js

---
+++ docs/src/core/PlayerController.js
@@ -0,0 +1,21 @@
+export class PlayerController {
+    constructor() {
+        this.position = {
+            x: 0,
+            y: 5,
+            z: 0
+        };
+
+        this.velocity = {
+            x: 0,
+            y: 0,
+            z: 0
+        };
+
+        this.isFlying = false;
+    }
+
+    update(deltaTime) {
+        // lógica futura
+    }
+}
